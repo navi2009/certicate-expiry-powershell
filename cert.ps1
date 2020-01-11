ad3266962989cefc0676013705f2d7fa92f6c667
navi2009 <navinnandakumar2091@gmail.com>
2020-01-11 08:25:58 +0530

    Add files via upload

---
+++ cert.ps1
@@ -0,0 +1,28 @@
+﻿$servers=Get-Content "C:\server.TXT"
+ 
+$result=@()
+ 
+foreach ($i in $servers)
+ 
+{
+ 
+$ErrorActionPreference="SilentlyContinue"
+ 
+$a=Invoke-Command -ComputerName $i {Get-ChildItem Cert:\LocalMachine\My -Recurse |
+Where-Object {$_ -is [System.Security.Cryptography.X509Certificates.X509Certificate2] -and $_.NotAfter -gt (Get-Date) -and $_.NotAfter -lt (Get-Date).AddDays(30)}
+ 
+}
+ 
+foreach ($c in $a) {
+ 
+$result+=New-Object -TypeName PSObject -Property ([ordered]@{
+'Server'=$i;
+'Certificate'=$c.Issuer;
+'Expires'=$c.NotAfter
+ 
+})
+ 
+}
+ 
+}
+$result | Out-File "C:\output.TXT"
